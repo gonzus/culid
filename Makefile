d32b21b152639186aecf1f0d1f93cd7d23eda129
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2024-12-03 13:07:06 +0100

    Allow compiling based off uint128 or struct

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ gonzo: gonzo.o
 	cc -std=c11 -Wall -Wextra -Wshadow -g -o gonzo gonzo.o
 
 gonzo.o: gonzo.c culid.h
-	cc -c -std=c11 -Wall -Wextra -Wshadow -g -o gonzo.o gonzo.c
+	cc -c -std=c11 -DCULID_FORCE_UINT128 -Wall -Wextra -Wshadow -g -o gonzo.o gonzo.c
+	# cc -c -std=c11 -DCULID_FORCE_STRUCT -Wall -Wextra -Wshadow -g -o gonzo.o gonzo.c
 
 clean:
 	rm -f gonzo gonzo.o
--- culid.h
+++ culid.h
@@ -1,11 +1,19 @@
 #pragma once
 
-#ifdef __SIZEOF_INT128__
-#define CULID_UINT128
+#if defined(CULID_FORCE_UINT128) && defined(CULID_FORCE_STRUCT)
+#error cannot define both CULID_FORCE_UINT128 and CULID_FORCE_STRUCT
 #endif
 
-#ifdef CULID_UINT128
+#if defined(CULID_FORCE_UINT128)
+#define CULID_USE_UINT128
+#elif defined(CULID_FORCE_STRUCT)
+#undef CULID_USE_UINT128
+#elif defined(__SIZEOF_INT128__)
+#define CULID_USE_UINT128
+#endif
+
+#if defined(CULID_USE_UINT128)
 #include "culid_uint128.h"
 #else
-#include "culid_struct.hh"
-#endif // ULID_UINT128
+#include "culid_struct.h"
+#endif
